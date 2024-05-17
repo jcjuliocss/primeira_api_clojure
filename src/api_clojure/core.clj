2329239261084fee1494f5611fe683dec705d563
Julio Cesar <julio.silva@softrh.com.br>
2024-05-17 11:08:56 -0300

    removendo site-defaults e usando somente o api-defaults, ja que nao sera preciso protecao de cross site request forgery (CSFR) por se tratar apenas de uma api que servira a outro proposito

--- src/api_clojure/core.clj
+++ src/api_clojure/core.clj
@@ -1,11 +1,11 @@
 (ns api-clojure.core
   (:require [ring.adapter.jetty :refer [run-jetty]]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults api-defaults]]
+            [ring.middleware.defaults :refer [wrap-defaults api-defaults]]
             [api-clojure.db :as db]
             [api-clojure.routes :refer [app-routes]]))
 
 (def app
-  (wrap-defaults app-routes (assoc-in api-defaults [:security :anti-forgery] false)))
+  (wrap-defaults app-routes api-defaults))
 
 (defn -main
   "Funcao principal."
